92e6ef5780eca91654adcfc6c7415fb01068000f
11618nathan <11618nathan@gmail.com>
2019-02-02 21:38:23 +0900

    Upload

---
+++ dataframe.R
@@ -0,0 +1,7 @@
+# data.frame - 데이터 프레임 나열하여 입력
+score <- data.frame(kor = c(95, 80, 75),
+                    en = c(70, 80, 90), 
+                    math = c(100, 60, 80))
+
+# dataframe 출력
+score 
